5187a74613584feffed30ecf9be739572e906b22
Rikuto Hayashi <ritu6811@gmail.com>
2019-07-26 20:29:01 +0900

    register update

--- public/javascripts/account/register.js
+++ public/javascripts/account/register.js
@@ -12,12 +12,19 @@ function pushRegister() {
     console.log("ユーザー名" + user);
     console.log("パスワード" + pass);
 
+    // ここでcsvに書き込むのを送る
+    if(user != null && pass != ""){
+        socket.emit("addUser", ("username" + user + ",password" + pass));
+
+    }else{
+        console.log("データが不十分だお");
+    }
     //    送られてきたユーザー情報をcsvに書き込んでログインページに切り替える
     var userName = escape(user);
     //    ＆で連結
     var pram = "user=" + userName;
 
-    location.href = "../users?" + pram;
+    // location.href = "../users?" + pram;
     return false;
     //    同一のユーザーがいた場合の処理も考えた方がイイのかもしれない。
 }
